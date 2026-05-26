b6b85d038a28cc61f6abb1fb4e5e8240cfeb61ef
Daniyal <daniyalkamran33@gmail.com>
2026-05-26 07:42:59 +0500

    LOL

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,12 @@
-// import express from 'express'
-
 const express = require('express');
 const app = express();
 
 const Port = 3001
 
+app.use(express.json())
+
 app.get('/health', (req,res) => {
- res.send('hello World')
+ res.json({ health : 'OK'})
 })
 
 app.listen(Port , () => {
---
+++ Pipeline.py
@@ -0,0 +1,3 @@
+CG-ewsFPRixGHNKPaKR9h4i2fbK
+
+https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin&x_cg_demo_api_key=CG-ewsFPRixGHNKPaKR9h4i2fbK
