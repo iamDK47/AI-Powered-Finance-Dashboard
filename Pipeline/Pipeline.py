b9411f7a87ad04fffd2ad78a9c32ee7c889fb840
Daniyal <daniyalkamran33@gmail.com>
2026-06-15 22:19:15 +0500

    a

--- Pipeline/Pipeline.py
+++ Pipeline/Pipeline.py
@@ -2,11 +2,21 @@ import requests
 import os
 
 beta_API = {'bitcoin' : {'usd' : 69420.96}}
-Mock_API = True
+Mock_API = False
 
 coins = ['bitcoin,ethereum,solana,cardano,ripple']
 result = ','.join(coins)
-The_url = f"https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids={result}"
+
+# The_url = f"https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids={result}"
+# The_url = "https://api.coingecko.com/api/v3/search?query=bitcoin"
+# The_url = "https://api.coingecko.com/api/v3/coins/list"
+# The_url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&names=Bitcoin&symbols=btc&category=layer-1&price_change_percentage=1h"
+# The_url = "https://api.coingecko.com/api/v3/coins/bitcoin"
+The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/tickers?exchange_ids=bybit"
+# The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2023"
+# The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1"
+# The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1711929600&to=1712275200"
+# The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd"
 
 custom_header = {
  "x-cg-demo-api-key": os.getenv('API_KEY')
@@ -17,6 +27,7 @@ try:
   if Mock_API:
     mock_result = beta_API['bitcoin']['usd']
     print(f"BETA API WORKING price is ${mock_result:,}")
+    print()
 
   else:
     response = requests.get(The_url, headers=custom_header)
