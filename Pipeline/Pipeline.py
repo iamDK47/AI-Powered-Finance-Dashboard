c23c37aa559526cd80bbb79c55fba35bea73f3d0
Daniyal <daniyalkamran33@gmail.com>
2026-06-24 20:05:56 +0500

    time

--- Pipeline/Pipeline.py
+++ Pipeline/Pipeline.py
@@ -1,5 +1,8 @@
 import requests
+import json
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 beta_API = {'bitcoin' : {'usd' : 69420.96}}
 Mock_API = False
@@ -13,10 +16,11 @@ result = ','.join(coins)
 # The_url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&names=Bitcoin&symbols=btc&category=layer-1&price_change_percentage=1h"
 # The_url = "https://api.coingecko.com/api/v3/coins/bitcoin"
 # The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/tickers?exchange_ids=bybit"
-# The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2023"
-# The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1"
+# The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/history?date=01-01-2026"
+The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=365"
 # The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1711929600&to=1712275200"
-The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd&days=1"
+# The_url = "https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd&days=1"
+# The_url = "https://api.coingecko.com/api/v3/search/trending"
 
 custom_header = {
  "x-cg-demo-api-key": os.getenv('API_KEY')
@@ -34,8 +38,9 @@ try:
 
     if response.status_code == 200:
       print("api is working")
-      response_json = response.json()
-      print(response_json)
+  
+      with open('response.json', 'w') as f:
+        json.dump(response.json(), f, indent=2)
 
     else:  
       print("failed to connect")
