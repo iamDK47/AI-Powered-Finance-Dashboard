4d2e4ac60168ec276f66029ecbd44d559b1335d4
Daniyal <daniyalkamran33@gmail.com>
2026-07-13 22:38:47 +0500

    sql

---
+++ Pipeline/pg.sql
@@ -0,0 +1,2 @@
+INSERT INTO global_crypto_data ('ticker','open','high','low','close','volume_COIN','volume_USDT','total_trades','market_buyer_volume','market_sell_volume','delta','close_Time')
+    VALUES("")
